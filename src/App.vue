204f218f39fc2af9679821d7eeed33bf522e157a
timothy okooboh <okoobohtimothy@gmail.com>
2021-05-03 23:16:10 +0100

    remove comments

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,6 @@
       @submit.prevent="submit"
       style="max-width: 600px"
     >
-      <!-- <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" v-model="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-      </div> -->
-
       <base-input
         type="email"
         required
